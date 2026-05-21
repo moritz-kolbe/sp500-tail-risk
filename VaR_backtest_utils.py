fca635d7c6a463a9201efdb9a7384410742c62de
Moritz Kolbe <kolbe.moritz@icloud.com>
2026-05-21 11:21:33 +0200

    add ES backtest utils

--- VaR_backtest_utils.py
+++ VaR_backtest_utils.py
@@ -24,19 +24,21 @@ def backtest_single(i):
         u = np.quantile(losses_z, 0.95)
         exceedances = losses_z[losses_z > u] - u
         if len(exceedances) < 20:
-            return np.nan, False
+            return np.nan, np.nan, False
 
         xi, _, beta = genpareto.fit(exceedances, floc=0)
         f_bar_u = len(exceedances) / len(losses_z)
         var_z = u + (beta / xi) * (((1 - 0.99) / f_bar_u) ** (-xi) - 1)
+        es_z = (var_z + beta - xi * u) / (1 - xi)
 
         forecast = result.forecast(horizon=1)
         sigma_next = np.sqrt(forecast.variance.values[-1, 0]) / 100
 
         var_return = var_z * sigma_next
+        es_return = es_z * sigma_next
         actual_loss = -losses_arr[i]
         violation = actual_loss > var_return
 
-        return var_return, violation
+        return var_return, es_return, violation
     except:
-        return np.nan, False
+        return np.nan, np.nan, False
